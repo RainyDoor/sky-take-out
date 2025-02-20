6c7cb004615bdaa3fe42e414a68b763ad469eac2
RaingDoor <3391182076@qq.com>
2025-02-20 16:22:54 +0800

    新增分类功能模块

---
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -0,0 +1,65 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/admin/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping
+    public Result addCategory(@RequestBody CategoryDTO categoryDTO) {
+        log.info("新增分类：{}", categoryDTO);
+        categoryService.addCategory(categoryDTO);
+        return Result.success();
+    }
+
+    @GetMapping("/page")
+    public Result<PageResult> page(CategoryPageQueryDTO categoryPageQueryDTO) {
+        log.info("分类分页查询：{}", categoryPageQueryDTO);
+        PageResult pageResult = categoryService.page(categoryPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    @PutMapping
+    public Result update(@RequestBody CategoryDTO categoryDTO) {
+        log.info("修改分类：{}", categoryDTO);
+        categoryService.update(categoryDTO);
+        return Result.success();
+    }
+
+    @PostMapping("/status/{status}")
+    public Result startOrStop(@PathVariable Integer status, Long id) {
+        log.info("启用、禁用分类：{}, {}", status, id);
+        categoryService.startOrStop(status, id);
+        return Result.success();
+    }
+
+    @GetMapping("/list")
+    public Result<List<Category>> list(Integer type) {
+        log.info("根据类型查询分类：{}", type);
+        List<Category> list = categoryService.list(type);
+        return Result.success(list);
+    }
+
+    @DeleteMapping
+    public Result delete(Long id) {
+        log.info("根据id删除分类: {}", id);
+        categoryService.delete(id);
+        return Result.success();
+    }
+
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,85 @@
+package com.sky.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.mapper.CategoryMapper;
+import com.sky.result.PageResult;
+import com.sky.service.CategoryService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    @Override
+    public void addCategory(CategoryDTO categoryDTO) {
+        Category category = new Category();
+        BeanUtils.copyProperties(categoryDTO, category);
+
+        //默认设置禁用
+        category.setStatus(StatusConstant.DISABLE);
+
+        category.setCreateTime(LocalDateTime.now());
+        category.setUpdateTime(LocalDateTime.now());
+        category.setCreateUser(BaseContext.getCurrentId());
+        category.setUpdateUser(BaseContext.getCurrentId());
+
+        categoryMapper.insert(category);
+    }
+
+    @Override
+    public PageResult page(CategoryPageQueryDTO categoryPageQueryDTO) {
+        LambdaQueryWrapper<Category> wrapper = new LambdaQueryWrapper<>();
+        wrapper.like(categoryPageQueryDTO.getName() != null, Category::getName, categoryPageQueryDTO.getName());
+        wrapper.eq(categoryPageQueryDTO.getType() != null, Category::getType, categoryPageQueryDTO.getType());
+        wrapper.orderByAsc(Category::getSort);
+
+        IPage<Category> iPage = new Page<>(categoryPageQueryDTO.getPage(), categoryPageQueryDTO.getPageSize());
+        categoryMapper.selectPage(iPage, wrapper);
+
+        return new PageResult(iPage.getTotal(), iPage.getRecords());
+    }
+
+    @Override
+    public void update(CategoryDTO categoryDTO) {
+        Category category = new Category();
+        BeanUtils.copyProperties(categoryDTO, category);
+
+        category.setUpdateTime(LocalDateTime.now());
+        category.setUpdateUser(BaseContext.getCurrentId());
+
+        categoryMapper.updateById(category);
+    }
+
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        Category category = Category.builder().id(id).status(status).build();
+        categoryMapper.updateById(category);
+    }
+
+    @Override
+    public List<Category> list(Integer type) {
+        LambdaQueryWrapper<Category> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Category::getType, type);
+        return categoryMapper.selectList(wrapper);
+    }
+
+    @Override
+    public void delete(Long id) {
+        categoryMapper.deleteById(id);
+    }
+
+}
